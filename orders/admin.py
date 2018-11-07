91e258427043e18f80332603c19a70d7e15add11
KG <graevskiy@gmail.com>
2018-11-07 18:45:49 +0300

    models added

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import Pizza, Topping, Sub
+
 # Register your models here.
+
+admin.site.register(Pizza)
+admin.site.register(Topping)
+admin.site.register(Sub)
--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Pizza(models.Model):
+    
+    name = models.CharField(max_length=64)
+    size = models.CharField(max_length=64)
+    price = models.FloatField()    
+
+    def __str__(self):
+        return f"{self.size} pizza (${self.price})"
+
+class Topping(models.Model):
+
+    name = models.CharField(max_length=128)    
+    pizzas = models.ManyToManyField(Pizza, blank=True, related_name="toppings")
+
+    def __str__(self):
+        return f"{self.name}"
+
+class Sub(models.Model):
+    name = models.CharField(max_length=64)
+    price = models.FloatField()
+
+    def __str__(self):
+        return f"{self.name}"
